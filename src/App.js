d57b584ee05a879acfea784825b65bb6a8b1877d
Natálie Mikešová <natalie.mikesova@bimproject.cz>
2021-05-21 00:10:30 +0200

    chore: setup create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+      <div className="dla">
+        <header className="login-app_header">
+          <h1>Demo login app</h1>
+        </header>
+      </div>
+  );
+}
+
+export default App;
